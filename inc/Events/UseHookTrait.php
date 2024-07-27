ca6adf7cb023020a0a5830b82017c9f671493b82
COQUARD Cyrille <coquardcyr@gmail.com>
2024-07-27 20:42:05 +0200

    Added fix for filters and actions

--- inc/Events/UseHookTrait.php
+++ inc/Events/UseHookTrait.php
@@ -11,13 +11,13 @@ trait UseHookTrait
     use EventDispatcherAwareTrait;
 
     protected function apply_filter(string $name, array $parameters = []): array {
-        $filter = new Hook($name, $parameters);
+        $filter = new Filter($name, $parameters);
         $filter = $this->event_dispatcher->dispatch($filter);
         return $filter->get_parameters();
     }
 
     protected function do_action(string $name, array $parameters): void  {
-        $action = new Hook($name, $parameters);
+        $action = new Action($name, $parameters);
         $this->event_dispatcher->dispatch($action);
     }
 }
